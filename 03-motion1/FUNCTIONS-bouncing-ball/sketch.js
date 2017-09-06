3d922366949ec1b95a2c295a9eb48713da36caeb
Douglas Fleischut <df@douglass-imac.lan>
2017-09-05 22:22:15 -0700

    one more little bit

--- 03-motion1/FUNCTIONS-bouncing-ball/sketch.js
+++ 03-motion1/FUNCTIONS-bouncing-ball/sketch.js
@@ -7,25 +7,43 @@ var x = 100;
 var y = 100;
 var xspeed = 2.5;
 var yspeed = 2;
-
-
+var numRectangles = 8;
+var rectangles = [];
 
 function setup() {
   createCanvas(640, 360);
+
+
+  for (var i=0; i<numRectangles; i++) {
+    rectangles[i]=new SmarterRectangle();
+  }
+
 }
 
 function draw() {
   background(51);
 
 
+for (var i=0; i<rectangles.length;i++) {
+  rectangles[i].drawRectangle();
+}
+
+
+// rectangles.forEach(function(rectangle) {
+//   rectangle.drawRectangle();
+// })
+
 
 // drawRectangle(200,200,100,150,[30,200,80]);
 
 
-for (var i=0; i <10;i++) {
+// for (var i=0; i <10;i++) {
 
-  new Rectangle(random(200),random(200),random(300),random(150),[30,200,80]);
-}
+//   new Rectangle(random(200),random(200),random(300),random(150),[30,200,80]);
+// }
+
+
+
 
 move();
 checkEdge();
@@ -47,6 +65,11 @@ function checkEdge() {
   // check Edges
   if ((x > width) || (x < 0)) {
     xspeed = xspeed * -1;
+
+    for (var i=0; i<rectangles.length;i++) {
+      rectangles[i].randomize();
+    }
+
   }
   if ((y > height) || (y < 0)) {
     yspeed = yspeed * -1;
@@ -79,6 +102,36 @@ var Rectangle = function(rectX,rectY,rectWidth,rectHeight, color) {
 
   fill(color);
   rect(rectX,rectY,rectWidth,rectHeight);
+
+
+
+}
+
+
+
+var SmarterRectangle = function() {
+    this.color = [random(255),random(255),random(255)];
+    this.rectX=random(200);
+    this.rectY=random(200);
+    this.rectWidth=random(400);
+    this.rectHeight=random(600);
+
+  this.randomize = function() {
+    this.color = [random(255),random(255),random(255)];
+    this.rectX=random(200);
+    this.rectY=random(200);
+    this.rectWidth=random(400);
+    this.rectHeight=random(600);
+}
+
+
+  this.drawRectangle = function() {
+    fill(this.color);
+    rect(this.rectX,this.rectY,this.rectWidth,this.rectHeight);
+
+  }
+
+
 
 
 
