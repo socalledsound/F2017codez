2cecd37c8e15e37e21f538c45138bcded2ffb9b0
socalledsound <lrs6000@gmail.com>
2017-10-11 16:00:09 -0700

    Add files via upload

--- 10-glitch/glitch-vid1-impressionistic/sketch.js
+++ 10-glitch/glitch-vid1-impressionistic/sketch.js
@@ -2,7 +2,7 @@ var vid1;
 
 function setup() {
   createCanvas(400, 400);
-  frameRate(3);
+  frameRate(30);
   vid1 = createVideo("ihop.mp4");
   vid1.loop();
   vid1.hide();
@@ -16,9 +16,9 @@ function setup() {
 function draw() {
 
   background(255);
-  image(vid1,0,0,400,400);
+  //image(vid1,0,0,400,400);
   vid1.loadPixels();
-   var stepSize = round(constrain(mouseX/8, 2, 10));
+   var stepSize = round(constrain(mouseX/50, 2, 40));
   // var stepSize=round(random(6,10));
   //var stepSize = 5;
   for (var y=0; y<height; y+=stepSize) {
@@ -26,9 +26,9 @@ function draw() {
       var index = (y * width + x)*4;
       var darkness = (255 - vid1.pixels[index]) / 255;
       var radius = stepSize+10 * darkness;
-      fill(vid1.pixels[index]*random(1,2),vid1.pixels[index+1]*random(1,2),vid1.pixels[index+2]*random(1,2),random(20));
+      fill(vid1.pixels[index]*random(1,2),vid1.pixels[index+1]*random(1,2),vid1.pixels[index+2]*random(1,2),random(200));
       //rect(x*14+random(2*y), y*random(10,14)+random(2*x), radius*random(2,4), radius*random(2,8));
-      ellipse(x*2, y*2, random(5,200), random(5,200));
+      rect(x*2, y*2, random(15,20), random(15,20));
     }
   }
 
